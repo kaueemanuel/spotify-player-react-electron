873e916ff473f093326968883c86e8fc87cc9419
Kauê Brasil <kaue.malheiros@olist.com>
2023-01-13 02:39:20 -0300

    feat: react electron base

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,21 @@ module.exports = {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'error',
-    // Since React 17 and typescript 4.1 you can safely disable the rule
-    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', 'tsx', 'ts'] },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
   parserOptions: {
     ecmaVersion: 2020,
@@ -21,10 +34,11 @@ module.exports = {
       webpack: {
         config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
       },
-      typescript: {},
+      typescript: { alwaysTryTypes: true },
     },
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
+    'import/extensions': ['error', 'always', { ignorePackages: true }],
   },
 };
